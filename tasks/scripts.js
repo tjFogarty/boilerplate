ad66583f4f72c6ee4d5dee3545b663a498219ac4
TJ Fogarty <tj@emagine.ie>
2015-03-08 12:12:38 +0000

    Upgrade dependencies and added optional es6 option

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -8,12 +8,27 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var notify = require('gulp-notify');
 var source = require('vinyl-source-stream');
-var es6ify = require('es6ify');
+var babelify = require('babelify');
 var config = require('../config');
 
 gulp.task('scripts', function () {
-  return browserify(es6ify.runtime)
-    .transform(es6ify)
+  if (config.js_es6) {
+    return browserify({debug: true})
+      .transform(babelify)
+      .add(config.js_entry_file)
+      .bundle()
+      .on('error', function (err) {
+        console.log(err.message);
+
+        this.emit('end');
+      })
+      .pipe(source('main.bundle.js'))
+      .pipe(gulp.dest(config.js_dir))
+      .pipe(reload({stream:true}))
+      .pipe(notify('JS compilation complete.'));
+  }
+
+  return browserify({debug: true})
     .add(config.js_entry_file)
     .bundle()
     .on('error', function (err) {
