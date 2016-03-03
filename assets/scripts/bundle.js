3b06c468ba877e7f74a217e2b7f43e1227e74aeb
TJ Fogarty <tj@emagine.ie>
2016-03-03 09:12:19 +0000

    Updates webpack notifier

--- assets/scripts/bundle.js
+++ assets/scripts/bundle.js
@@ -55,17 +55,18 @@
 
 	var _foundationSites = __webpack_require__(3);
 
+	/**
+	 * We're using Webpack, which doesn't like libs that don't export as a module
+	 * Until more devs get on board, whatever isn't supported is compiled into a vendors file
+	 * See webpack.config.js to see how this is done
+	 */
 	(function ($) {
 	  // When the DOM is ready, we can start up any plugins or custom scripts
 	  $(document).ready(function () {
 	    // Pretty much everything you code goes in here
 	    $(document).foundation();
 	  });
-	})(jQuery); /**
-	             * We're using Webpack, which doesn't like libs that don't export as a module
-	             * Until more devs get on board, whatever isn't supported is compiled into a vendors file
-	             * See webpack.config.js to see how this is done
-	             */
+	})(jQuery);
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ },
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,10 @@
+import { Foundation } from 'foundation-sites';
+
 /**
  * We're using Webpack, which doesn't like libs that don't export as a module
  * Until more devs get on board, whatever isn't supported is compiled into a vendors file
  * See webpack.config.js to see how this is done
  */
-
-import { Foundation } from 'foundation-sites';
-
 (function($) {
   // When the DOM is ready, we can start up any plugins or custom scripts
   $(document).ready(function() {
