8affc3ec5f4549cecd6d9a813e70100d91bfefef
T.J. Fogarty <fogarty.tj@gmail.com>
2016-05-19 23:04:21 +0100

    Adds Emagine favicon

--- _header.php
+++ _header.php
@@ -6,7 +6,7 @@
     <title>Boilerplate</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" href="/favicon.ico?v=1.1"> 
     <link rel="stylesheet" href="/assets/css/main.css">
   </head>
   <body>
