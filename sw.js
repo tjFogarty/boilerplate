6ed32313e6c23d70ffa6a0cb62561aacaf000d10
T.J. Fogarty <fogarty.tj@gmail.com>
2016-07-14 20:54:00 +0100

    Adds service worker example

---
+++ sw.js
@@ -0,0 +1,30 @@
+// taken from: https://remysharp.com/2016/03/22/the-copy--paste-guide-to-your-first-service-worker
+// we'll version our cache (and learn how to delete caches in
+// some other post)
+const cacheName = 'v1::static';
+
+self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        /*
+          DEAR READER,
+          ADD A LIST OF YOUR ASSETS THAT
+          YOU WANT TO WORK WHEN OFFLINE
+          TO THIS ARRAY OF URLS
+        */
+      ]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
